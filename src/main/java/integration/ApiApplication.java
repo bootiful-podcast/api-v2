2519165ed753dc79b1ba8e5661cee1126cf31d1e
Josh Long <josh@joshlong.com>
2020-10-28 18:49:59 -0700

    api

--- src/main/java/integration/ApiApplication.java
+++ src/main/java/integration/ApiApplication.java
@@ -36,70 +36,66 @@ public class ApiApplication {
 	RabbitMqHelper rabbitMqHelper(AmqpAdmin amqpAdmin) {
 		return new RabbitMqHelper(amqpAdmin);
 	}
-
-	// todo remove this
-	@Bean
-	CachingConnectionFactory rabbitConnectionFactoryPreferred(RabbitProperties properties,
-			ObjectProvider<ConnectionNameStrategy> connectionNameStrategy) throws Exception {
-
-		PropertyMapper map = PropertyMapper.get();
-		CachingConnectionFactory factory = new CachingConnectionFactory(
-				getRabbitConnectionFactoryBean(properties).getObject());
-		map.from(properties::determineAddresses).to(factory::setAddresses);
-		map.from(properties::isPublisherReturns).to(factory::setPublisherReturns);
-		map.from(properties::getPublisherConfirmType).whenNonNull().to(factory::setPublisherConfirmType);
-		RabbitProperties.Cache.Channel channel = properties.getCache().getChannel();
-		map.from(channel::getSize).whenNonNull().to(factory::setChannelCacheSize);
-		map.from(channel::getCheckoutTimeout).whenNonNull().as(Duration::toMillis)
-				.to(factory::setChannelCheckoutTimeout);
-		RabbitProperties.Cache.Connection connection = properties.getCache().getConnection();
-		map.from(connection::getMode).whenNonNull().to(factory::setCacheMode);
-		map.from(connection::getSize).whenNonNull().to(factory::setConnectionCacheSize);
-		map.from(connectionNameStrategy::getIfUnique).whenNonNull().to(factory::setConnectionNameStrategy);
-		return factory;
-	}
-
-	private RabbitConnectionFactoryBean getRabbitConnectionFactoryBean(RabbitProperties properties) throws Exception {
-		System.out.println(properties.toString());
-		PropertyMapper map = PropertyMapper.get();
-		RabbitConnectionFactoryBean factory = new RabbitConnectionFactoryBean();
-		map.from(properties::determineHost).whenNonNull().to(factory::setHost);
-		map.from(properties::determinePort).to(factory::setPort);
-		map.from(properties::determineUsername).whenNonNull().to(factory::setUsername);
-		map.from(properties::determinePassword).whenNonNull().to(factory::setPassword);
-		map.from(properties::determineVirtualHost).whenNonNull().to(factory::setVirtualHost);
-		map.from(properties::getRequestedHeartbeat).whenNonNull().asInt(Duration::getSeconds)
-				.to(factory::setRequestedHeartbeat);
-		map.from(properties::getRequestedChannelMax).to(factory::setRequestedChannelMax);
-		RabbitProperties.Ssl ssl = properties.getSsl();
-		if (ssl.determineEnabled()) {
-			factory.setUseSSL(true);
-			map.from(ssl::getAlgorithm).whenNonNull().to(factory::setSslAlgorithm);
-			map.from(ssl::getKeyStoreType).to(factory::setKeyStoreType);
-			map.from(ssl::getKeyStore).to(factory::setKeyStore);
-			map.from(ssl::getKeyStorePassword).to(factory::setKeyStorePassphrase);
-			map.from(ssl::getTrustStoreType).to(factory::setTrustStoreType);
-			map.from(ssl::getTrustStore).to(factory::setTrustStore);
-			map.from(ssl::getTrustStorePassword).to(factory::setTrustStorePassphrase);
-			map.from(ssl::isValidateServerCertificate)
-					.to((validate) -> factory.setSkipServerCertificateValidation(!validate));
-			map.from(ssl::getVerifyHostname).to(factory::setEnableHostnameVerification);
-		}
-		map.from(properties::getConnectionTimeout).whenNonNull().asInt(Duration::toMillis)
-				.to(factory::setConnectionTimeout);
-		factory.afterPropertiesSet();
-
-		var tsc = new ToStringCreator(properties);
-		System.out.println(tsc.toString());
-		System.out.println("addresses: " + properties.determineAddresses());
-		System.out.println("host: " + properties.determineHost());
-		System.out.println("pw: " + properties.determinePassword());
-		System.out.println("username : " + properties.determineUsername());
-		System.out.println("vhost: " + properties.determineVirtualHost());
-		System.out.println("port: " + properties.determinePort());
-
-		return factory;
-	}
+	/*
+	 * // todo remove this
+	 *
+	 * @Bean CachingConnectionFactory rabbitConnectionFactoryPreferred(RabbitProperties
+	 * properties, ObjectProvider<ConnectionNameStrategy> connectionNameStrategy) throws
+	 * Exception {
+	 *
+	 * PropertyMapper map = PropertyMapper.get(); CachingConnectionFactory factory = new
+	 * CachingConnectionFactory( getRabbitConnectionFactoryBean(properties).getObject());
+	 * map.from(properties::determineAddresses).to(factory::setAddresses);
+	 * map.from(properties::isPublisherReturns).to(factory::setPublisherReturns);
+	 * map.from(properties::getPublisherConfirmType).whenNonNull().to(factory::
+	 * setPublisherConfirmType); RabbitProperties.Cache.Channel channel =
+	 * properties.getCache().getChannel();
+	 * map.from(channel::getSize).whenNonNull().to(factory::setChannelCacheSize);
+	 * map.from(channel::getCheckoutTimeout).whenNonNull().as(Duration::toMillis)
+	 * .to(factory::setChannelCheckoutTimeout); RabbitProperties.Cache.Connection
+	 * connection = properties.getCache().getConnection();
+	 * map.from(connection::getMode).whenNonNull().to(factory::setCacheMode);
+	 * map.from(connection::getSize).whenNonNull().to(factory::setConnectionCacheSize);
+	 * map.from(connectionNameStrategy::getIfUnique).whenNonNull().to(factory::
+	 * setConnectionNameStrategy); return factory; }
+	 *
+	 * private RabbitConnectionFactoryBean getRabbitConnectionFactoryBean(RabbitProperties
+	 * properties) throws Exception { System.out.println(properties.toString());
+	 * PropertyMapper map = PropertyMapper.get(); RabbitConnectionFactoryBean factory =
+	 * new RabbitConnectionFactoryBean();
+	 * map.from(properties::determineHost).whenNonNull().to(factory::setHost);
+	 * map.from(properties::determinePort).to(factory::setPort);
+	 * map.from(properties::determineUsername).whenNonNull().to(factory::setUsername);
+	 * map.from(properties::determinePassword).whenNonNull().to(factory::setPassword);
+	 * map.from(properties::determineVirtualHost).whenNonNull().to(factory::setVirtualHost
+	 * ); map.from(properties::getRequestedHeartbeat).whenNonNull().asInt(Duration::
+	 * getSeconds) .to(factory::setRequestedHeartbeat);
+	 * map.from(properties::getRequestedChannelMax).to(factory::setRequestedChannelMax);
+	 * RabbitProperties.Ssl ssl = properties.getSsl(); if (ssl.determineEnabled()) {
+	 * factory.setUseSSL(true);
+	 * map.from(ssl::getAlgorithm).whenNonNull().to(factory::setSslAlgorithm);
+	 * map.from(ssl::getKeyStoreType).to(factory::setKeyStoreType);
+	 * map.from(ssl::getKeyStore).to(factory::setKeyStore);
+	 * map.from(ssl::getKeyStorePassword).to(factory::setKeyStorePassphrase);
+	 * map.from(ssl::getTrustStoreType).to(factory::setTrustStoreType);
+	 * map.from(ssl::getTrustStore).to(factory::setTrustStore);
+	 * map.from(ssl::getTrustStorePassword).to(factory::setTrustStorePassphrase);
+	 * map.from(ssl::isValidateServerCertificate) .to((validate) ->
+	 * factory.setSkipServerCertificateValidation(!validate));
+	 * map.from(ssl::getVerifyHostname).to(factory::setEnableHostnameVerification); }
+	 * map.from(properties::getConnectionTimeout).whenNonNull().asInt(Duration::toMillis)
+	 * .to(factory::setConnectionTimeout); factory.afterPropertiesSet();
+	 *
+	 * var tsc = new ToStringCreator(properties); System.out.println(tsc.toString());
+	 * System.out.println("addresses: " + properties.determineAddresses());
+	 * System.out.println("host: " + properties.determineHost());
+	 * System.out.println("pw: " + properties.determinePassword());
+	 * System.out.println("username : " + properties.determineUsername());
+	 * System.out.println("vhost: " + properties.determineVirtualHost());
+	 * System.out.println("port: " + properties.determinePort());
+	 *
+	 * return factory; }
+	 */
 
 	@Bean
 	RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
