28ec9190f2c1cc5fda4e1018aa287266e190f7d5
Josh Long <josh@joshlong.com>
2020-11-01 20:22:22 -0800

    invalidation

---
+++ bin/invalidate_indexes.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+U=$1
+P=$2
+H=${3:-http://api.admin.bootifulpodcast.online}
+E=0
+MESSAGE="Please specify a "
+
+if [  "$U" = ""  ]; then
+  echo $MESSAGE username
+  E=1
+fi
+
+if [ "$P" = "" ]; then
+  echo $MESSAGE password
+  E=1
+fi
+
+echo "Authenticating with '$U' / '$P'"
+
+if [[ $E == 1  ]]; then
+  echo "Usage: $0 <USERNAME> <PASSWORD>"
+  exit 1;
+ fi
+
+HOST=$H
+curl -H"Authorization: bearer $(curl -XPOST -u jlong:pw ${HOST}/token  > /dev/null 2>&1 )" -XDELETE ${HOST}/admin/caches  > /dev/null 2>&1
+
