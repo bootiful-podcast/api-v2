256a3d4c389521cea7df1b3c6c8b11510b3c8168
Josh Long <josh@joshlong.com>
2020-12-06 23:39:47 -0800

    up

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -24,45 +24,6 @@ image_id=$(docker images -q api)
 docker tag "${image_id}" gcr.io/${GCLOUD_PROJECT}/${APP_NAME}
 docker push gcr.io/${GCLOUD_PROJECT}/${APP_NAME}
 
-#kubectl delete secrets ${SECRETS} || echo "could not delete ${SECRETS}."
-#kubectl delete -f $API_YAML || echo "could not delete the existing Kubernetes environment as described in ${API_YAML}."
-#kubectl apply -f <(echo "
-#---
-#apiVersion: v1
-#kind: Secret
-#metadata:
-#  name: ${SECRETS}
-#type: Opaque
-#stringData:
-#  BP_MODE: ${BP_MODE_LOWERCASE}
-#  SPRING_RABBITMQ_USERNAME: ${BP_RABBITMQ_MANAGEMENT_USERNAME}
-#  SPRING_RABBITMQ_PASSWORD: ${BP_RABBITMQ_MANAGEMENT_PASSWORD}
-#  SPRING_RABBITMQ_HOST: rabbitmq
-#  SPRING_PROFILES_ACTIVE: cloud
-#  SENDGRID_API_KEY: "${SENDGRID_API_KEY}"
-#  SPRING_DATASOURCE_USERNAME: ${PSQL_USER}
-#  SPRING_DATASOURCE_PASSWORD: ${PSQL_PW}
-#  SPRING_DATASOURCE_URL:  jdbc:postgresql://postgres:5432/bp
-#  AWS_ACCESS_KEY: "${AWS_ACCESS_KEY_ID}"
-#  AWS_ACCESS_KEY_SECRET: "${AWS_SECRET_ACCESS_KEY}"
-#  AWS_REGION: "${AWS_REGION}"
-#  PODBEAN_CLIENT_SECRET: "${PODBEAN_CLIENT_SECRET}"
-#  PODBEAN_CLIENT_ID: "${PODBEAN_CLIENT_ID}"
-#  PODCAST_PIPELINE_S3_INPUT_BUCKET_NAME: "${PODCAST_PIPELINE_S3_INPUT_BUCKET_NAME}"
-#  PODCAST_PIPELINE_S3_OUTPUT_BUCKET_NAME: "${PODCAST_PIPELINE_S3_OUTPUT_BUCKET_NAME}"
-#")
-##podcast.pipeline.s3.input-bucket-name=podcast-input-bucket
-##podcast.pipeline.s3.output-bucket-name=podcast-output-bucket
-#
-#kubectl apply -f $API_YAML
-#
-## kubectl get service | grep $APP_NAME || kubectl apply -f $API_SERVICE_YAML
-#
-#kubectl apply -f $API_SERVICE_YAML
-#
-#kubectl rollout restart deployments/api
-
-
 export RESERVED_IP_NAME=${APP_NAME}-${BP_MODE_LOWERCASE}-ip
 gcloud compute addresses list --format json | jq '.[].name' -r | grep $RESERVED_IP_NAME ||
   gcloud compute addresses create $RESERVED_IP_NAME --global
